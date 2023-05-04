1fd63dddf1ed716fce0e5b49b91c009f7b19e3b6
AmalStudy <amalengstudy@gmail.com>
2023-05-04 01:41:58 -0700

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,31 +1,10 @@
 const express = require('express');
-
-// import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
-
-// import authMiddleware from utils
-const { authMiddleware } = require('./utils/auth');
-
-// import typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// create a new Apollo server and pass in our schema data
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  // This ensures that every request performs an authentication check, and the updated request object will be passed to the resolvers as the context
-  context: authMiddleware
-});
-
-// integrate our apollo server with the Express application middleware
-server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -38,9 +17,5 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`);
-    // log where we can go to test the GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
